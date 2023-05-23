789fab4320abd5ae4b35d43edfa044385eae378b
Huy Dep Trai <huyxxgd@gmail.com>
2023-05-23 12:32:58 +0700

    first commit

---
+++ PHONE.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  akira
+ *    created: 04/09/2022 19:06:41
+**/
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void solve() {
+    int x; cin >> x;
+    int t; cin >> t;
+    cout << t*x;
+}
+
+int main() {
+    solve();
+    return 0;
+}
