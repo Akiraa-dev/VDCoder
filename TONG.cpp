18d50669e184fb21ab6ff4781c7e819dc3fba5fa
Huy Dep Trai <huyxxgd@gmail.com>
2023-05-23 12:20:30 +0700

    first commit

---
+++ TONG.cpp
@@ -0,0 +1,19 @@
+/**
+ *    author:  akira
+ *    created: 04/07/2022 13:39:27
+**/
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void solve() {
+    ll a; cin >> a;
+    ll b; cin >> b;
+    ll sum = a + b;
+    cout << sum << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
